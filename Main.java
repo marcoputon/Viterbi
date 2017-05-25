c60df576dc0d8e2d20a8f2a651353dd06c4c1ae8
marcoputon <marcoputon@gmail.com>
2017-05-24 21:50:36 -0300

    Tratando os valores passados na execução

--- Main.java
+++ Main.java
@@ -4,6 +4,18 @@ import java.util.*;
 
 public class Main{
 	public static void main(String[] args){
+		try{
+			float ruido = Float.valueOf(args[0]);
+			System.out.println("Ruido: " + ruido);
+			String entrada = args[1];
+			System.out.println("Entrada: " + entrada);
+		}
+		catch(Exception e){
+			System.out.println("erro: entrada inválida");
+			System.exit(-1);
+		}
+
+
         StatesMachine sm = new StatesMachine(args[1]);
         sm.run();
     }
